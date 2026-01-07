bc4bf7fdee3bcb6456480f64d981c7a3da070506
HandoA01 <chikomhi4@gachon.ac.kr>
2026-01-07 20:15:33 +0900

    feat : Kakao Button 컴포넌트 구현

---
+++ src/components/Kakao/Kakao.styles.ts
@@ -0,0 +1,7 @@
+export const kakaoBase = 
+  "inline-flex items-center justify-center rounded-full transition-all duration-200 focus:outline-none whitespace-nowrap active:opacity-80";
+
+export const kakaoLayout = {
+  container: "w-[335px] h-[45px] px-[14px] gap-x-2",
+  text: "text-[15px] font-medium tracking-tight"
+};
