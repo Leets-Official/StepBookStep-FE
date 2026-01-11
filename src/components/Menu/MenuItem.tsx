08f9bf763eef6a75cab4db657765859fcc70da38
HandoA01 <chikomhi4@gachon.ac.kr>
2026-01-11 17:11:04 +0900

    feat : Menu 컴포넌트 구현

---
+++ src/components/Menu/MenuItem.tsx
@@ -0,0 +1,23 @@
+import { itemBase, itemActive, itemInactive, itemLabel, itemIcon } from "./Menu.styles";
+import { ChevronRightIcon } from "@/assets/icons";
+
+interface MenuItemProps {
+  label: string;
+  interaction?: boolean;
+  onClick?: () => void;
+}
+
+export function MenuItem({ label, interaction = false, onClick }: MenuItemProps) {
+  return (
+    <li 
+      className={[
+        itemBase,
+        interaction ? itemActive : itemInactive
+      ].join(" ")} 
+      onClick={onClick}
+    >
+      <span className={itemLabel}>{label}</span>
+      <ChevronRightIcon className={itemIcon} />
+    </li>
+  );
+}
