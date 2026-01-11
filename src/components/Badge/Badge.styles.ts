0ce7ee89aa6d47722450369d441184a468dfad3b
chaemin <cheamin0038@gmail.com>
2026-01-11 23:37:27 +0900

    feat : Badge 컴포넌트 타입 추가

--- src/components/Badge/Badge.styles.ts
+++ src/components/Badge/Badge.styles.ts
@@ -1,10 +1,8 @@
-/* ===== 기존 태그 키워드 (절대 유지) ===== */
 export const contain =
   "inline-flex px-1.5 py-0.5 text-xs leading-xs font-sb justify-center items-center rounded-full bg-purple-100";
 
 export const text = "text-purple-500";
 
-/* ===== Lv 배지 추가 ===== */
 export const lvContain =
   "inline-flex px-4 h-8 justify-center items-center rounded-full bg-lime-200";
 
