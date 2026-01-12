b198961c488b50e36c05ef985921df21cbab28b6
baeys0619 <baeys2005@gmail.com>
2026-01-12 22:54:59 +0900

    style: 스타일 정의를 TailwindCSS 표준문법으로 수정

--- src/components/AppBar/AppBar.styles.ts
+++ src/components/AppBar/AppBar.styles.ts
@@ -1,17 +1,17 @@
 export const appBarStyles = {
-  container: "w-[375px] h-[44px] py-[10px] bg-white flex justify-center items-center box-content",
+  container: "w-[375px] h-11 py-2.5 bg-white flex justify-center items-center box-content",
 
-  innerWrapper: "w-[335px] h-[24px] flex items-center",
+  innerWrapper: "w-[335px] h-6 flex items-center",
 
   // 로고 없는 거
-  titleModeWrapper: "gap-[12px]",
+  titleModeWrapper: "gap-3",
 
   // 로고 있는 거 
   logoModeWrapper: "justify-between",
 
-  icon: "w-[24px] h-[24px] cursor-pointer text-gray-500",
+  icon: "w-6 h-6 cursor-pointer text-gray-500",
 
-  logoImage: "w-[128px] h-[20px]",
+  logoImage: "w-32 h-5",
 
-  titleText: "text-black font-['Pretendard'] font-semibold text-[16px] leading-[20px] tracking-[0px]",
+  titleText: "text-black font-['Pretendard'] font-semibold text-base leading-5 tracking-normal",
 };
