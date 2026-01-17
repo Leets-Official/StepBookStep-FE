055c567a9810e39b2a338bced51fdbfcc1b2b175
chaemin <cheamin0038@gmail.com>
2026-01-17 09:58:03 +0900

    refactor : Badge 스타일 및 타입 변경

--- src/components/Badge/Badge.styles.ts
+++ src/components/Badge/Badge.styles.ts
@@ -1,19 +1,7 @@
-export const contain =
-  "inline-flex px-2 py-0.5 text-xs leading-xs font-medium justify-center items-center rounded-full bg-white border border-gray-300";
+export const baseContain = "inline-flex items-center justify-center rounded-full border border-2";
 
-export const text = "text-gray-500";
+export const baseText = "font-semibold leading-none";
 
-export const lv1Contain =
-  "inline-flex px-1.5 py-0.5 justify-center items-center rounded-full bg-lime-200";
+export const levelText = "text-md px-2 py-0.5";
 
-export const lv1Text = "text-sm font-semibold text-purple-500";
-
-export const lv2Contain =
-  "inline-flex px-1.5 py-0.5 justify-center items-center rounded-full bg-lime-300";
-
-export const lv2Text = "text-sm font-semibold text-purple-800";
-
-export const lv3Contain =
-  "inline-flex px-1.5 py-0.5 justify-center items-center rounded-full bg-lime-400";
-
-export const lv3Text = "text-sm font-semibold text-purple-800";
+export const tagText = "text-sm px-2.5 py-1";
