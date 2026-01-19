30f7ab57097170037fc09d98ce8dfd973b01f231
HandoA01 <chikomhi4@gachon.ac.kr>
2026-01-19 16:49:26 +0900

    chore : error 해결

--- src/pages/PlayGround.tsx
+++ src/pages/PlayGround.tsx
@@ -1,27 +1,3 @@
-<<<<<<< Updated upstream
-import StateCarousel from "@/components/StateCarousel/StateCarousel";
-import type{ ReadingStatus } from "@/components/StateCarousel/StateCarousel.types";
-import { useState } from "react";
-
-const PlayGround = () => {
-  const [, setCurrentStatus] = useState<ReadingStatus>("BEFORE");
-
-  const handleStatusChange = (newStatus: ReadingStatus) => {
-    setCurrentStatus(newStatus);
-    console.log("변경된 독서 상태:", newStatus);
-  };
-
-  return (
-    <div className="min-h-screen flex flex-col items-center justify-center gap-12">
-
-      {/* 테스트할 컴포넌트 */}
-        <StateCarousel 
-          initialStatus="BEFORE" 
-          onChange={handleStatusChange} 
-        />
-
-    </div>
-=======
 import { ReadingStateDetail } from "@/components/ReadingStateDetail/ReadingStateDetail";
 import { MOCK_READING_DATA, MOCK_COMPLETED_DATA } from "@/mocks/readingState.mock";
 export default function PlayGround() {
@@ -38,8 +14,5 @@ export default function PlayGround() {
             <ReadingStateDetail data={MOCK_COMPLETED_DATA} />
           </section>
         </div>
->>>>>>> Stashed changes
   );
 };
-
-export default PlayGround;
