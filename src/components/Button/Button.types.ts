064a177e2a5d6258669c6fe0a448779827690f53
chaemin <cheamin0038@gmail.com>
2026-01-23 20:43:45 +0900

    chore: Button 타입 정의 코드 정리

--- src/components/Button/Button.types.ts
+++ src/components/Button/Button.types.ts
@@ -3,11 +3,11 @@ import React from "react";
 export type ButtonSize = "large" | "medium" | "small";
 
 export type Variant =
-  | "primary"          // Purple 채움
-  | "primaryOutline"   // Purple 테두리 
-  | "secondary"        // Lime 채움
+  | "primary" // Purple 채움
+  | "primaryOutline" // Purple 테두리
+  | "secondary" // Lime 채움
   | "secondaryOutline" // Lime 테두리
-  | "ghost";           // 배경 없음
+  | "ghost"; // 배경 없음
 
 export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   label: string;
