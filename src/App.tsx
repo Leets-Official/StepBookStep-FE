ee284335552da706acf03d04b5d34c4e30c9b88c
chaemin <cheamin0038@gmail.com>
2026-01-23 20:49:56 +0900

    feat: 온보딩 플로우 라우팅 구조 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-import PlayGround from "./pages/PlayGround";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+
+import SetProfile from "@/pages/Onboarding/SetProfile";
+import OnboardingLevelStep1 from "@/pages/Onboarding/OnboardingLevelStep1";
+import OnboardingLevelStep2 from "@/pages/Onboarding/OnboardingLevelStep2";
+import OnboardingLevelStep3 from "@/pages/Onboarding/OnboardingLevelStep3";
+import OnboardingGenre from "@/pages/Onboarding/OnboardingGenre.tsx";
 
 function App() {
-  return <PlayGround />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Navigate to="/onboarding/set-profile" />} />
+        <Route path="/onboarding/set-profile" element={<SetProfile />} />
+        <Route path="/onboarding/level/step-1" element={<OnboardingLevelStep1 />} />
+        <Route path="/onboarding/level/step-2" element={<OnboardingLevelStep2 />} />
+        <Route path="/onboarding/level/step-3" element={<OnboardingLevelStep3 />} />
+        <Route path="/onboarding/genre" element={<OnboardingGenre />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
