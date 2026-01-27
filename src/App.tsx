ee4c1d7c5582e834358b7940bcc32c4926041402
chaemin <cheamin0038@gmail.com>
2026-01-28 00:14:54 +0900

    feat : 홈 화면 페이지 경로 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import SetProfile from "@/pages/Onboarding/SetProfile";
 import OnboardingLevelStep1 from "@/pages/Onboarding/OnboardingLevelStep1";
@@ -7,12 +7,13 @@ import OnboardingLevelStep3 from "@/pages/Onboarding/OnboardingLevelStep3";
 import OnboardingGenre from "@/pages/Onboarding/OnboardingGenre";
 import RoutineResultPage from "@/pages/Onboarding/RoutineResultPage";
 import BookDetailPage from "@/pages/BookDetail/BookDetailPage";
+import Home from "@/pages/Home/Home.tsx";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Navigate to="/onboarding/set-profile" />} />
+        <Route path="/" element={<Home />} />
         <Route path="/onboarding/set-profile" element={<SetProfile />} />
         <Route path="/onboarding/level/step-1" element={<OnboardingLevelStep1 />} />
         <Route path="/onboarding/level/step-2" element={<OnboardingLevelStep2 />} />
