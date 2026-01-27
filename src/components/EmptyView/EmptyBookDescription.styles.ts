79c7816e7d8e977bdc2533efa4692520eadbd2fb
chaemin <cheamin0038@gmail.com>
2026-01-28 01:18:55 +0900

    feat : 책 소개 엠티 뷰 구현

---
+++ src/components/EmptyView/EmptyBookDescription.styles.ts
@@ -0,0 +1,36 @@
+export const container = `
+  flex flex-col
+  items-center
+  justify-center
+  py-8
+  text-center
+`;
+
+export const iconWrapper = `
+  relative
+  w-[96px]
+  h-[74px]
+  mb-4
+`;
+
+export const bubble = `
+  w-full
+  h-full
+`;
+
+export const question = `
+  absolute
+  top-2/5
+  left-1/2
+  -translate-x-1/2
+  -translate-y-1/2
+  w-[24px]
+  h-[38px]
+`;
+
+export const text = `
+  text-lg
+  font-medium
+  text-gray-300
+  leading-6
+`;
