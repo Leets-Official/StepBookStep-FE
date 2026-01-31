ee2f631c2ee8060ca73282b2e98782d53e9189e7
baeys0619 <baeys2005@gmail.com>
2026-01-31 18:09:09 +0900

    design/fix : TailwindCSS 문법으로 수정/npm install rechart실행

--- src/pages/BookDetail/components/FinishedModal/FinishedModal.styles.ts
+++ src/pages/BookDetail/components/FinishedModal/FinishedModal.styles.ts
@@ -3,19 +3,19 @@ export const overlay =
 
 export const container = `
   w-[375px] h-[458px] bg-white flex flex-col items-center 
-  rounded-t-[24px] pt-[40px] px-[20px] pb-[40px] gap-[12px] 
+  rounded-t-3xl pt-10 px-5 pb-10 gap-3 
   opacity-100 shadow-2xl relative
 `;
 
-export const gifImage = "w-[240px] h-[240px] object-contain opacity-100";
+export const gifImage = "w-60 h-60 object-contain";
 
 export const textWrapper = "flex flex-col items-center gap-2 text-center";
 
-export const title = "text-[24px] font-bold text-gray-900";
+export const title = "text-2xl font-bold text-gray-900";
 
 export const highlight = "text-purple-600";
 
-export const subTitle = "text-[14px] text-gray-500 leading-relaxed font-medium";
+export const subTitle = "text-sm text-gray-500 leading-relaxed font-medium";
 
 export const linkButton =
-  "mt-auto text-[12px] text-purple-400 hover:underline underline-offset-4 cursor-pointer";
+  "mt-auto text-xs text-purple-400 hover:underline underline-offset-4 cursor-pointer";
